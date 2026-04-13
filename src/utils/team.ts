93541b6efa972e1eff152cfa0f0a0c2acd8b49f3
Farouk <faizfarouk2015@gmail.com>
2026-04-13 10:29:01 +0200

    resume in actions

--- src/utils/team.ts
+++ src/utils/team.ts
@@ -6,12 +6,14 @@ export const team = {
     displayName: "Farouk Faiz",
     image: farouk,
     url: "https://www.linkedin.com/in/faizfarouk/",
+    resumeUrl: "/resume/farouk.pdf",
     email: "farouk@qasten.com",
   },
   achraf: {
     displayName: "Achraf El Masdouri",
     image: achraf,
     url: "https://www.linkedin.com/in/achraf-el-masdouri-68675a174/",
+    resumeUrl: "/resume/achraf.pdf",
     email: "achraf@qasten.com",
   },
 } as const;
